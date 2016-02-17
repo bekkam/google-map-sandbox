642bc21d02816eef7630d2fc5b731ecb863981a2
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-02-17 08:56:43 -0800

    Add server, add templates for simple directions and draggable directions

---
+++ server.py
@@ -0,0 +1,18 @@
+"""A flask app for practice"""
+
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def show_map():
+    return render_template('map-draggable-directions.html')
+
+
+# map directions simple - no text directions
+# map draggable directions - text directions, waypoints, draggable.
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
