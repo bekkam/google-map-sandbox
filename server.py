4393d5c75548887c07eb905f2d96e17ac317a198
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-02-17 15:58:56 -0800

    Modify server and map-draggable-directions to process user inputs; need to fix map display

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 """A flask app for practice"""
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 
 app = Flask(__name__)
@@ -8,7 +8,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def show_map():
-    return render_template('directions-result.html')
+    return render_template('map-draggable-directions.html')
+
+
+@app.route('/submit-addresses.json', methods=['GET'])
+def get_addresses():
+    """Return addresses"""
+
+    start = request.args.get("start")
+    end = request.args.get("end")
+    return jsonify({"start_point": start, "end_point": end})
+
 
 
 # map directions simple - no text directions
